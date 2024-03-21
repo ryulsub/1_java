56185ed94bafe0aa30afd1754ba6f5b86bf13895
ryulsub <142034848+ryulsub@users.noreply.github.com>
2024-03-21 15:09:54 +0900

    Add files via upload

--- lesson01/Quiz04.java
+++ lesson01/Quiz04.java
@@ -15,16 +15,16 @@ public class Quiz04 {
 //		출력 예시
 //		몫 : 105 나머지 : 3
 		
-		Scanner scan = new Scanner(System.in);	
-		
-		System.out.print("입력1: " ); 
-		int count = scan.nextInt();
-		System.out.print("입력2: ");
-		int count2 = scan.nextInt();
-		
-		System.out.println("몫: " + count / count2);
-		
-		System.out.println("나머지: " + count % count2);
+//		Scanner scan = new Scanner(System.in);	
+//		
+//		System.out.print("입력1: " ); 
+//		int count = scan.nextInt();
+//		System.out.print("입력2: ");
+//		int count2 = scan.nextInt();
+//		
+//		System.out.println("몫: " + count / count2);
+//		
+//		System.out.println("나머지: " + count % count2);
 		
 System.out.println("------------------------------------------------------------------------");
 
@@ -36,7 +36,7 @@ System.out.println("------------------------------------------------------------
 //		
 //		출력 예시
 //		x는 4이고, y는 6 입니다.
-
+//
 //		Scanner scan = new Scanner(System.in);
 //		
 //		System.out.print("x :" );
@@ -45,7 +45,7 @@ System.out.println("------------------------------------------------------------
 //		int y = scan.nextInt();
 //		
 //		System.out.println("x는" + " 4이고, " + "y는" + " 6입니다." );
-		
+//		
 System.out.println("----------------------------------------------------------------------");	
 
 //		3. 초 변환
@@ -61,11 +61,58 @@ System.out.println("------------------------------------------------------------
 //		
 //		System.out.print("초 :" );
 //		int sec = scan.nextInt();
-//		int sec1 = 464 / 60;
-//		int sec2 = 464 % 60;
-//		System.out.println(sec1 + "분 " + sec2 + "초 ");
 //		
-		
+//		int sec1 = sec / 60;
+//		System.out.print(sec1 + "분 ");
+//		
+//		int sec2 = sec % 60;
+//		System.out.println(sec2 + "초 ");
+
+System.out.println("------------------------------------------");
+
+	
+//		4. 자리수 쪼개기
+//		네 자리수 숫자를 입력 받아서 천의 자리부터 한줄씩 출력 하세요.
+//		입력 예시
+//		입력 : 1234
+//		
+//		출력 예시
+//		1
+//		2
+//		3
+//		4
+
+//	 	Scanner scan = new Scanner(System.in);
+//	 	
+//	 	System.out.print("입력 : ");
+//	 	int num = scan.nextInt();
+//	 	
+//	 	int num1 = scanner.next();
+//	 	int num2 = sacn.next();
+//	 	
+//	 	
+//	 	System.out.println();
+//	 	System.out.println();
+//	 	System.out.println();
+//	 	System.out.println();
+//	 	
+	 	
+System.out.println("--------------------------------------------");
+
+
+//		5. 자리수 합 구하기
+//		네 자리수를 입력 받아서 각 자리수의 합을 출력하세요.
+//		입력 예시
+//		입력 : 1234
+//		
+//		출력 예시
+//		합계는 10 입니다. 
+
+	 			
+	 	
+
+
+
 	}
 
 }
