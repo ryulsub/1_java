442175c29a653493a37b5089d9523a2d0cd3e783
ryulsub <142034848+ryulsub@users.noreply.github.com>
2024-04-04 18:44:39 +0900

    Add files via upload

--- lesson02/quiz01/Rectangle.java
+++ lesson02/quiz01/Rectangle.java
@@ -2,13 +2,14 @@ package lesson02.quiz01;
 
 public class Rectangle {
 	// 필드
-	// 어떻게 셋팅할 것인가?		생성자
+	// 어떻게 셋팅할 것인가?		1. 생성자  2. setter
 	private int width;
 	private int height;
 	
 	
 	// 메소드
 	// 생성자 - 필드를 셋팅
+	// 객체가 생성되는 시점에 한번만 호출
 	public Rectangle(int width, int height) {
 		this.width = width;
 		this.height = height;
@@ -17,10 +18,28 @@ public class Rectangle {
 				+ "cm 사각형이 만들어졌습니다.");
 	}
 	
+	// SETTER
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	
 	// 넓이 구하기
 	public int getArea() {
 		return this.width * this.height;
 		
 	}
+	
+
+	// 둘레 구하기
+	public int getRound() {
+		return (this.width + this.height) * 2;
+	}
+	
 	
 }
--- lesson02/quiz01/RectangleTest.java
+++ lesson02/quiz01/RectangleTest.java
@@ -3,9 +3,21 @@ package lesson02.quiz01;
 public class RectangleTest {
 
 	public static void main(String[] args) {
+		
 		Rectangle rect1 = new Rectangle(30, 50);
 		System.out.println("넓이: " + rect1.getArea() + "cm^2");
+		System.out.println("둘레: " + rect1.getRound()+ "cm");
+		
+		Rectangle rect2 = new Rectangle(10, 10);
+		
+		// 사각형의 크기 변경
+		rect2.setWidth(50);
+		rect2.setHeight(50);
+		System.out.println("넓이:" + rect2.getArea() + "cm^2");
+		System.out.println("둘레:" +rect2.getRound() + "cm");
+		
 		
 	}
-
+	
+	
 }
